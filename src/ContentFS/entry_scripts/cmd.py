0907fca8132842022eaf7ed01fbb7ac8a0072a7d
Md. Sabuj Sarker <md.sabuj.sarker@gmail.com>
2020-10-05 22:51:26 +0600

    cmd script updated

--- src/ContentFS/entry_scripts/cmd.py
+++ src/ContentFS/entry_scripts/cmd.py
@@ -4,22 +4,43 @@ from ContentFS.ctrees.crootdirtree import CRootDirTree
 from ContentFS.cpaths.cfile_hashed import CFileHashed
 
 
+def main_list(root):
+    path_infos = []
+    for cpath in root.get_descendant_cpaths():
+        path_infos.append(
+            f"-{'D' if cpath.is_dir() else 'F'}> {cpath.path}{'    [size(' + str(cpath.size) + ') mtime(' + str(cpath.mtime) + ')]' if cpath.is_file() else ''}{' - hash(' + cpath.hash + ')' if isinstance(cpath, CFileHashed) else ''}")
+    for path_info in path_infos:
+        print(path_info)
+
+
+def main_json(root):
+    print(root.to_json())
+
+
 def main():
+    """
+    --hash: with file hashed
+    --dev-matcher: dev fs matcher will be used - that is .git/ folders will be ignored
+    --json: json output will be printed instead of list.
+    """
     args = sys.argv[1:]
     print(f'ARGS: {args}')
     base_path = os.getcwd()
     print(f'base_path: {base_path}')
-    root = CRootDirTree(base_path)#.with_dev_matcher()
+    root = CRootDirTree(base_path)
+    if '--dev-matcher' in sys.argv[1:]:
+        root.with_dev_matcher()
+
     if '--hash' in args:
         do_hash = True
     else:
         do_hash = False
     root.load(do_hash=do_hash)
-    path_infos = []
-    for cpath in root.get_descendant_cpaths():
-        path_infos.append(f"-{'D' if cpath.is_dir() else 'F'}> {cpath.path}{'    [size(' + str(cpath.size) + ') mtime(' + str(cpath.mtime) + ')]' if cpath.is_file() else ''}{' - hash(' + cpath.hash + ')' if isinstance(cpath, CFileHashed) else ''}")
-    for path_info in path_infos:
-        print(path_info)
+
+    if '--json' in sys.argv[1:]:
+        main_json(root)
+    else:
+        main_list(root)
 
 
 if __name__ == '__main__':
